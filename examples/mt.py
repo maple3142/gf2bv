fcb3db645c7df6b1c02712768481788a7768dbef
maple3142 <kirby741852963@gmail.com>
2024-10-17 14:20:07 +0800

    update mt example to get one solution

--- examples/mt.py
+++ examples/mt.py
@@ -1,9 +1,10 @@
-import random
+import random, time
 from gf2bv import LinearSystem
 from gf2bv.crypto.mt import MT19937
 
 
 def mt19937(bs):
+    print("bs:", bs)
     rand = random.Random()
     st = tuple(rand.getstate()[1][:-1])
 
@@ -16,14 +17,16 @@ def mt19937(bs):
     rng = MT19937(mt)
     zeros = [rng.getrandbits(bs) ^ o for o in out] + [mt[0] ^ 0x80000000]
     print("solving...")
-    for sol in lin.solve_all(zeros):
-        print("solved", sol[:10])
-        assert sol == st
+    start = time.time()
+    sol = lin.solve_one(zeros)
+    print("time:", time.time() - start)
+    print("solved", sol[:10])
+    assert sol == st
 
-        rng = MT19937(sol)
-        pyrand = rng.to_python_random()
-        assert all(rng.getrandbits(bs) == o for o in out)
-        assert all(pyrand.getrandbits(bs) == o for o in out)
+    rng = MT19937(sol)
+    pyrand = rng.to_python_random()
+    assert all(rng.getrandbits(bs) == o for o in out)
+    assert all(pyrand.getrandbits(bs) == o for o in out)
 
 
 if __name__ == "__main__":
