270183ffcb125e33880bce3eace766c1283dab7c
maple3142 <kirby741852963@gmail.com>
2024-10-18 00:40:23 +0800

    add quadratic system

---
+++ examples/nlfsr.py
@@ -0,0 +1,68 @@
+from gf2bv import QuadraticSystem
+from gf2bv.crypto.lfsr import GaloisLFSR, FibonacciLFSR
+from tqdm import tqdm
+import secrets, itertools
+
+
+n, mask = 128, 0xD670201BAC7515352A273372B2A95B23
+select = (13, 24, 35, 46, 57)
+
+
+def combiner(x0, x1, x2, x3, x4):
+    # this combining function is even: 50% of the time it will return 1
+    return (x0 * x1) ^ (x0 * x1 * x3 * x4) ^ x0 ^ x1 ^ x2
+
+
+def non_linear_output(lfsr):
+    lfsr()
+    x0, x1, x2, x3, x4 = [(lfsr.state >> i) & 1 for i in select]
+    return combiner(x0, x1, x2, x3, x4)
+
+
+def annihilator(x0, x1, x2, x3, x4):
+    # you can find this using sage.crypto.boolean_function.BooleanFunction
+    return (x0 * x1) ^ x0 ^ (x1 * x2) ^ x1 ^ x2 ^ 1
+
+
+def sanity_check():
+    for x0, x1, x2, x3, x4 in itertools.product([0, 1], repeat=5):
+        if combiner(x0, x1, x2, x3, x4) == 1:
+            assert annihilator(x0, x1, x2, x3, x4) == 0
+
+
+def nlfsr_test(LFSR):
+    # this example shows how QuadraticSystem can be used to solve a non-linear LFSR by linearization
+    print(f"Testing {LFSR.__name__}")
+    init = secrets.randbits(n)
+    print(f"{init = :0{n}b}")
+    lfsr = LFSR(n, mask, init)
+
+    N = 2**14 + 1000
+    out = [non_linear_output(lfsr) for _ in range(N)]
+
+    qsys = QuadraticSystem([128])
+    (x,) = qsys.gens()
+    lfsr_sys = LFSR(128, mask, x)
+    zeros = []
+    for o in tqdm(out):
+        lfsr_sys()
+        if o == 1:
+            x0, x1, x2, x3, x4 = [lfsr_sys.state.bits[i] for i in select]
+            # this is same as the annihilator function
+            z = qsys.mul_bit(x0, x1) ^ x0 ^ qsys.mul_bit(x1, x2) ^ x1 ^ x2 ^ 1
+            zeros.append(z)
+    print(len(zeros))
+    sols = list(qsys.solve_all(zeros))
+    for (sol,) in sols:
+        print(f"{sol = :0{n}b}")
+        assert sol == init
+
+    (sol,) = qsys.solve_one(zeros)
+    print(f"{sol = :0{n}b}")
+    assert sol == init
+
+
+if __name__ == "__main__":
+    sanity_check()
+    nlfsr_test(GaloisLFSR)
+    nlfsr_test(FibonacciLFSR)
--- gf2bv/__init__.py
+++ gf2bv/__init__.py
@@ -3,33 +3,35 @@ from .pym4ri import solve
 
 
 class BitVec:
-    __slots__ = ("_sys", "_st")
+    __slots__ = ("sys", "bits")
 
-    def __init__(self, sys: "LinearSystem", st: list[int]):
-        self._sys = sys
-        self._st = st
+    def __init__(self, sys: "LinearSystem", bits: list[int]):
+        self.sys = sys
+        # this represent the symbolic bits of the BitVec
+        # in little-endian order (lsb first)
+        self.bits = bits
 
     def __copy__(self):
-        return BitVec(self._sys, self._st[:])
+        return BitVec(self.sys, self.bits[:])
 
     def __len__(self):
-        return len(self._st)
+        return len(self.bits)
 
     def _cast_int(self, n: int):
         if n < 0:
             raise ValueError("Negative value")
-        st = [0] * len(self._st)
-        for i in range(len(self._st)):
+        st = [0] * len(self.bits)
+        for i in range(len(self.bits)):
             st[i] = n & 1
             n >>= 1
-        return BitVec(self._sys, st)
+        return BitVec(self.sys, st)
 
     def _check_sys(self, other: "BitVec"):
-        if self._sys is not other._sys:
+        if self.sys is not other.sys:
             raise ValueError("Cannot mix bitvecs from different systems")
 
     def _check_len(self, other: "BitVec"):
-        if len(self._st) != len(other._st):
+        if len(self.bits) != len(other.bits):
             raise ValueError("Cannot mix bitvecs of different lengths")
 
     def __xor__(self, other: Union["BitVec", int]):
@@ -38,7 +40,7 @@ class BitVec:
         else:
             self._check_sys(other)
             self._check_len(other)
-        return BitVec(self._sys, [a ^ b for a, b in zip(self._st, other._st)])
+        return BitVec(self.sys, [a ^ b for a, b in zip(self.bits, other.bits)])
 
     __rxor__ = __xor__
 
@@ -47,80 +49,82 @@ class BitVec:
         return self ^ other
 
     def __rshift__(self, n: int):
-        return BitVec(self._sys, self._st[n:] + [0] * n)
+        return BitVec(self.sys, self.bits[n:] + [0] * n)
 
     def __lshift__(self, n: int):
-        return BitVec(self._sys, [0] * n + self._st[:-n])
+        return BitVec(self.sys, [0] * n + self.bits[:-n])
 
     def __and__(self, mask: int):
         if mask < 0:
             raise ValueError("Negative value")
-        vecs = self._st[:]
-        for i in range(len(self._st)):
+        vecs = self.bits[:]
+        for i in range(len(self.bits)):
             if mask & 1 == 0:
                 vecs[i] = 0
             mask >>= 1
-        return BitVec(self._sys, vecs)
+        return BitVec(self.sys, vecs)
 
     __rand__ = __and__
 
     def __or__(self, mask: int):
         if mask < 0:
             raise ValueError("Negative value")
-        vecs = self._st[:]
-        for i in range(len(self._st)):
+        vecs = self.bits[:]
+        for i in range(len(self.bits)):
             if mask & 1:
                 vecs[i] = 1
             mask >>= 1
-        return BitVec(self._sys, vecs)
+        return BitVec(self.sys, vecs)
 
     __ror__ = __or__
 
     def rotr(self, n: int):
-        return BitVec(self._sys, self._st[n:] + self._st[:n])
+        return BitVec(self.sys, self.bits[n:] + self.bits[:n])
 
     def rotl(self, n: int):
-        return BitVec(self._sys, self._st[-n:] + self._st[:-n])
+        return BitVec(self.sys, self.bits[-n:] + self.bits[:-n])
 
     def sum(self):
         t = 0
-        for x in self._st:
+        for x in self.bits:
             t ^= x
-        return BitVec(self._sys, [t])
+        return BitVec(self.sys, [t])
 
     def zeroext(self, n: int):
-        return BitVec(self._sys, self._st + [0] * n)
+        return BitVec(self.sys, self.bits + [0] * n)
 
     def signext(self, n: int):
-        return BitVec(self._sys, self._st + [self._st[-1]] * n)
+        return BitVec(self.sys, self.bits + [self.bits[-1]] * n)
 
     def __getitem__(self, i: Union[int, slice]):
         if isinstance(i, slice):
-            return BitVec(self._sys, self._st[i])
-        return BitVec(self._sys, [self._st[i]])
+            return BitVec(self.sys, self.bits[i])
+        return BitVec(self.sys, [self.bits[i]])
 
     def dup(self, n: int):
-        return BitVec(self._sys, self._st * n)
+        return BitVec(self.sys, self.bits * n)
 
     def concat(self, other: "BitVec"):
         self._check_sys(other)
-        return BitVec(self._sys, self._st + other._st)
+        return BitVec(self.sys, self.bits + other.bits)
+
+
+Zeros = list[Union[BitVec, int]]
 
 
 class LinearSystem:
     def __init__(self, sizes: list[int]):
         self._sizes = sizes[:]
-        self._vars = []
+        self._vars: list[BitVec] = []
         i = 1  # lsb used to represent constant term (affine part)
         for size in self._sizes:
-            # vecs = list(mat[i : i + size])
             st = [1 << j for j in range(i, i + size)]
             i += size
             self._vars.append(BitVec(self, st))
         self._vars = tuple(self._vars)
 
     def gens(self):
-        return tuple(x.__copy__() for x in self._vars)
+        return self._vars
 
     def get_sage_mat(self, zeros: list[BitVec], tqdm=lambda x, desc: x):
         """
@@ -148,18 +152,23 @@ class LinearSystem:
             i += 1
         return mat, affine
 
-    def get_eqs(self, zeros: list[BitVec]):
+    def get_eqs(self, zeros: Zeros):
         """
         Convert zeros into a list of equations accepted by pym4ri.solve
         """
         # https://stackoverflow.com/questions/716477/join-list-of-lists-in-python/21034265#21034265
         # this seems to be the fastest way to flatten a list of lists
         eqs: list[int] = []
-        list(map(eqs.extend, (bv._st for bv in zeros)))
+        list(
+            map(
+                eqs.extend,
+                (bv.bits if isinstance(bv, BitVec) else [bv] for bv in zeros),
+            )
+        )
         eqs = list(filter(None, eqs))  # remove literal zeros
         return eqs
 
-    def solve_raw(self, zeros: list[BitVec], all: bool):
+    def solve_raw(self, zeros: Zeros, all: bool):
         eqs = self.get_eqs(zeros)
         if 1 in eqs:
             # no solution
@@ -172,7 +181,7 @@ class LinearSystem:
         # one solution mode: return the solution directly if it exists, otherwise return None
         return solve(eqs, cols, all)
 
-    def convert_sol(self, s: int):
+    def convert_sol(self, s: int) -> Union[tuple[int], None]:
         sol = []
         for size in self._sizes:
             sol.append(s & ((1 << size) - 1))
@@ -180,15 +189,86 @@ class LinearSystem:
         assert s == 0, "Invalid solution"
         return tuple(sol)
 
-    def solve_all(self, zero_bvs: list[BitVec]):
-        it = self.solve_raw(zero_bvs, True)
+    def solve_all(self, zeros: Zeros):
+        it = self.solve_raw(zeros, True)
         if it is None:
             return
         for s in it:
-            yield self.convert_sol(s)
+            ret = self.convert_sol(s)
+            if ret is not None:
+                yield ret
 
-    def solve_one(self, zero_bvs: list[BitVec]):
-        sol = self.solve_raw(zero_bvs, False)
+    def solve_one(self, zeros: Zeros):
+        sol = self.solve_raw(zeros, False)
         if sol is None:
             return
         return self.convert_sol(sol)
+
+
+class QuadraticSystem(LinearSystem):
+    def __init__(self, sizes: list[int]):
+        n = sum(sizes)
+        quad_terms = n * (n - 1) // 2
+        super().__init__(sizes + [quad_terms])
+        self._lin_size = n
+        self._quad_size = quad_terms
+
+    def gens(self):
+        return super().gens()[:-1]
+
+    def mul_bit(self, a: int, b: int):
+        n = self._lin_size
+        const_lin_mask = (1 << (1 + n)) - 1
+        # constant term and linear terms (x^2 = x in GF(2))
+        v = (a & const_lin_mask) & b
+        # quadratic terms
+        a >>= 1
+        b >>= 1
+        abits = [0] * n
+        bbits = [0] * n
+        for i in range(n):
+            abits[i] = a & 1
+            bbits[i] = b & 1
+            a >>= 1
+            b >>= 1
+        # assert a == b == 0, "a, b is not linear terms"
+        qbv = super().gens()[-1]
+        mi = 0
+        for i in range(n):
+            for j in range(i):
+                r = (abits[i] & bbits[j]) ^ (abits[j] & bbits[i])
+                if r:
+                    v |= qbv.bits[mi]  # same as (1 << (1 + n + mi))
+                mi += 1
+        # assert (v >> (1 + self._lin_size + self._quad_size)) == 0, "Overflow"
+        return v
+
+    def _check_lin_match_quad(self, lin: int, quad: int):
+        n = self._lin_size
+        lin_bits = [0] * n
+        for i in range(n):
+            lin_bits[i] = lin & 1
+            lin >>= 1
+        assert lin == 0, "Invalid linear part"
+        for i in range(n):
+            for j in range(i):
+                r = lin_bits[i] & lin_bits[j]
+                if r != (quad & 1):
+                    return False
+                quad >>= 1
+        assert quad == 0, "Invalid quadratic part"
+        return True
+
+    def convert_sol(self, s: int) -> tuple[int] | None:
+        lin = s & ((1 << self._lin_size) - 1)
+        s >>= self._lin_size
+        quad = s & ((1 << self._quad_size) - 1)
+        s >>= self._quad_size
+        assert s == 0, "Invalid solution"
+        if self._check_lin_match_quad(lin, quad):
+            return super().convert_sol(lin)[:-1]
+
+    def solve_one(self, zeros: list[BitVec | int]):
+        # we can't use the LinearSystem.solve_one because the returned solution might not pass convert_sol
+        for sol in self.solve_all(zeros):
+            return sol
