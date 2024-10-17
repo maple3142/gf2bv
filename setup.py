5baaab8c5fa5a8c1fdeb77f40efa4d2700a46815
maple3142 <kirby741852963@gmail.com>
2024-10-17 19:04:58 +0800

    update metadata

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, Extension
 
-module = Extension(
-    "gf2bv.pym4ri",
-    sources=["gf2bv/pym4ri.c"],
-    libraries=["m4ri"],
-    extra_compile_args=["-O3", "-march=native", "-mtune=native"],
-)
 
 setup(
     name="gf2bv",
-    version="0.1.0",
-    description="Solving linear systems over GF(2) by manipulating bitvectors",
     packages=["gf2bv", "gf2bv.crypto"],
-    ext_modules=[module],
+    ext_modules=[
+        Extension(
+            "gf2bv.pym4ri",
+            sources=["gf2bv/pym4ri.c"],
+            libraries=["m4ri"],
+            extra_compile_args=["-O3", "-march=native", "-mtune=native"],
+        )
+    ],
 )
